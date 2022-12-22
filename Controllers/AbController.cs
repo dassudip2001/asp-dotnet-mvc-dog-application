6f4a8d46a04dd52bbdfb3d244fcf624e96a9363c
Sudip Das <sudip5428@gmail.com>
2022-12-22 22:20:13 +0530

    create dog and show the dog

--- Controllers/AbController.cs
+++ Controllers/AbController.cs
@@ -7,12 +7,30 @@ namespace dotnet.Controllers;
 
 public class AbController : Controller
 {
+
+    private static List<DogViewModel> dogs= new List<DogViewModel>();
     public IActionResult Index()
     {
-        DogViewModel dogMode = new DogViewModel()
-        {
-            Name="rocket",Age=2
-        };
-        return View(dogMode);
+        // DogViewModel dogMode = new DogViewModel()
+        // {
+        //     Name="rocket",Age=2
+        // };
+        return View(dogs);
     }
+
+    // When i can click nav ber
+    public IActionResult Create()
+    {
+
+        var dognew=new DogViewModel();
+        return View(dognew);
+    }
+    
+    // create a new Dog
+    public IActionResult CreateDog(DogViewModel dogViewModel)
+    {
+        dogs.Add(dogViewModel);
+        return RedirectToAction(nameof(Index));
+    }
+
 }
